2e873f198a6085edddd2041a7cfc41b2716802f5
hongteacher <isabel8282@gmail.com>
2022-04-29 11:52:12 +0900

    first commit

---
+++ src/SliderData.js
@@ -0,0 +1,16 @@
+const SLIDES = [
+    {
+        id : 0,
+        url : 'img/slide1.png'
+    },
+    {
+        id : 1,
+        url : 'img/slide2.png'
+    },
+    {
+        id : 2,
+        url : 'img/slide3.png'
+    },
+]
+    
+export default SLIDES;
